56acd3acafd3c6fa7b2ddf3bd7fac9029212b9d9
Carl D'Oleo-Lundgren <carl.doleo.lundgren@gmail.com>
2020-04-27 14:44:50 -0500

    cleaned up code

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ const gameBoard = (() => {
 
     const buttons = (() => {
         reset = document.querySelector('.reset-btn button');
-        humanGame = document.querySelector('#human-btn');
+        multiplayerGame = document.querySelector('#human-btn');
         AIGame = document.querySelector('#AI-btn');
-        return {reset, humanGame, AIGame}
+        return {reset, multiplayerGame, AIGame}
     })();
 
     const plays = (() => {
@@ -24,7 +24,8 @@ const gameBoard = (() => {
             [0,4,8], //top left to bottom right
             [2,4,6] // top right to bottom left
         ];
-        return {x, o, winningCombos}
+        currentBoard = findCurrentBoard();
+        return {x, o, winningCombos, currentBoard}
     })();
 
     const gameStatus = (() => {
@@ -33,68 +34,192 @@ const gameBoard = (() => {
         isMultiplayer = null;
         return {started, won, isMultiplayer} 
     })();
+
     
     function checkIfXWins() {
-        if (plays.x.length < 3) {
-          return;
-        }
+        if (plays.x.length < 3) return;
+        
         for (let i = 0; i < plays.winningCombos.length; i++) {
-          let win = true;
-          for (let j = 0; j < plays.winningCombos[i].length; j++) {
-            if (!plays.x.includes(plays.winningCombos[i][j])) {
-              win = false;
-              break;
+            let win = true;
+            for (let j = 0; j < plays.winningCombos[i].length; j++) {
+                if (!plays.x.includes(plays.winningCombos[i][j])) {
+                    win = false;
+                    break;
+                }
             }
-          }
-          if (win) {
-            gameStatus.won = true;
-            document.querySelector('h2').innerText = `${player1} wins!`
-            document.querySelector('h2').classList.remove('hidden');
-            break;
-          }
+            if (win) {
+                gameStatus.won = true;
+                document.querySelector('h2').innerText = `${player1} wins!`;
+                document.querySelector('h2').classList.remove('hidden');
+                break;
+            }
+            checkIfTied(win);
         }
     };
-
+    
     function checkIfOWins() {
-        if (plays.o.length < 3) {
-          return;
-        }
+        if (plays.o.length < 3) return;
+        
         for (let i = 0; i < plays.winningCombos.length; i++) {
-          let win = true;
-          for (let j = 0; j < plays.winningCombos[i].length; j++) {
-            if (!plays.o.includes(plays.winningCombos[i][j])) {
-              win = false;
-              break;
+            let win = true;
+            for (let j = 0; j < plays.winningCombos[i].length; j++) {
+                if (!plays.o.includes(plays.winningCombos[i][j])) {
+                    win = false;
+                    break;
+                }
             }
-          }
-          if (win) {
-            gameStatus.won = true;
-            document.querySelector('h2').innerText = `${player2} wins!`
-            document.querySelector('h2').classList.remove('hidden');
-            break;
-          }
+            if (win) {
+                gameStatus.won = true;
+                document.querySelector('h2').innerText = `${player2} wins!`;
+                document.querySelector('h2').classList.remove('hidden');
+                break;
+            }
+            checkIfTied(win);
         }
     };
-
-    boardCell.forEach( (cell) => {
-        cell.addEventListener('click', function() {
-            if (cell.innerText != '' || gameStatus.won || !gameStatus.started) {
-                return;
-            } else if (plays.last == null || plays.last == 'O') {
-                cell.innerText = 'X'
-                plays.last = 'X'
-                plays.x.push(Number(cell.dataset.index));
-                plays.x.sort();
-            } else {
-                cell.innerText = 'O'
-                plays.last = 'O'
-                plays.o.push(Number(cell.dataset.index));
-                plays.o.sort();
-            }
+    
+    function checkIfTied(win) {
+        if (plays.x.length + plays.o.length == 9 && !win) {
+            document.querySelector('h2').innerText = `Tie!`
+            document.querySelector('h2').classList.remove('hidden');
+        }
+    }
+    
+    function runMultiplayerGame(cell) {
+        if (cell.innerText != '' || gameStatus.won || !gameStatus.started) {
+            return;
+        } else if (plays.last == null || plays.last == 'O') {
+            cell.innerText = 'X';
+            plays.last = 'X';
+            plays.x.push(Number(cell.dataset.index));
+            plays.x.sort();
+            checkIfXWins()
+        } else {
+            cell.innerText = 'O';
+            plays.last = 'O';
+            plays.o.push(Number(cell.dataset.index));
+            plays.o.sort();
+            checkIfOWins()
+        }
+    }
+    
+    function runAIGame(cell) {
+        if (cell.innerText != '' || gameStatus.won || !gameStatus.started) {
+            return;
+        } else { // run code for human click
+            cell.innerText = 'X';
+            plays.x.push(Number(cell.dataset.index));
+            plays.x.sort();
             checkIfXWins();
-            checkIfOWins();
-        })
-    })
+            plays.currentBoard = findCurrentBoard();
+
+            if (!gameStatus.won) { // run code for AI move
+                let bestMove = minimax(plays.currentBoard, 'O');
+                boardCell.forEach((cell) => {
+                    if (cell.dataset.index == bestMove.index) {
+                        cell.innerText = 'O'
+                        plays.o.push(Number(cell.dataset.index))
+                        plays.o.sort();
+                    }
+                });
+                checkIfOWins();
+            }
+        }
+    }
+
+    function findCurrentBoard() {
+        let arr = [];
+        for (i = 0; i < boardCell.length; i++) {
+            if (boardCell[i].innerText == '') {
+                arr.push(boardCell[i].dataset.index)
+            } else {
+                arr.push(boardCell[i].innerText)
+            }
+        }
+        return arr;
+    }
+
+    function winning(board, player){
+        if (
+               (board[0] == player && board[1] == player && board[2] == player) ||
+               (board[3] == player && board[4] == player && board[5] == player) ||
+               (board[6] == player && board[7] == player && board[8] == player) ||
+               (board[0] == player && board[3] == player && board[6] == player) ||
+               (board[1] == player && board[4] == player && board[7] == player) ||
+               (board[2] == player && board[5] == player && board[8] == player) ||
+               (board[0] == player && board[4] == player && board[8] == player) ||
+               (board[2] == player && board[4] == player && board[6] == player)
+            ) {
+                return true;
+            } else {
+                return false;
+            }
+    } 
+
+    function minimax(board, player){
+       let emptyCellsIndexes = board.filter(s => s != "O" && s != "X");
+        
+        if (winning(board, 'X')) {
+            return {score: -10};
+        } else if (winning(board, 'O')) {
+            return {score: 10};
+        } else if (emptyCellsIndexes.length === 0){
+            return {score: 0};
+        }
+        
+        let moves = [];
+        
+        for (let i = 0; i < emptyCellsIndexes.length; i++) {
+            let move = {};
+            move.index = board[emptyCellsIndexes[i]];
+        
+            board[emptyCellsIndexes[i]] = player;
+        
+            if (player == 'O'){
+                let result = minimax(board, 'X');
+                move.score = result.score;
+            } else {
+                let result = minimax(board, 'O');
+                move.score = result.score;
+            }
+        
+            board[emptyCellsIndexes[i]] = move.index;     //reset the spot to empty
+        
+            moves.push(move);
+        }
+        
+        let bestMove;
+        
+        if (player === 'O') {
+            let bestScore = -10000;
+            for (let i = 0; i < moves.length; i++){
+                if (moves[i].score > bestScore){
+                    bestScore = moves[i].score;
+                    bestMove = i;
+                }
+            }
+        } else { 
+            let bestScore = 10000;
+            for(let i = 0; i < moves.length; i++){
+                if (moves[i].score < bestScore){
+                    bestScore = moves[i].score;
+                    bestMove = i;
+                }
+            }
+        }
+        
+        return moves[bestMove];
+    }
+      
+    boardCell.forEach( (cell) => {
+        cell.addEventListener('click', () => {
+            if (gameStatus.isMultiplayer) {
+                runMultiplayerGame(cell);
+            } else {
+                runAIGame(cell);
+            }
+        });
+    });
 
     buttons.reset.addEventListener('click', () => {
         boardCell.forEach( (cell) => {
@@ -113,22 +238,21 @@ const gameBoard = (() => {
 
         player1 = null;
         player2 = null;
+    });
 
-    })
-
-    buttons.humanGame.addEventListener('click', () => {
+    buttons.multiplayerGame.addEventListener('click', () => {
         document.querySelector('.start-btns').classList.add('hidden');
         document.querySelector('.multiplayer-game-input').classList.remove('hidden');
         gameStatus.isMultiplayer = true;
         startGame();
-    })
+    });
 
     buttons.AIGame.addEventListener('click', () => {
         document.querySelector('.start-btns').classList.add('hidden');
         document.querySelector('.AI-game-input').classList.remove('hidden');
         gameStatus.isMultiplayer = false;
         startGame();
-    })
+    });
 
     const startGame = function () {
         const startGameBtn = document.querySelectorAll('.start-game')
